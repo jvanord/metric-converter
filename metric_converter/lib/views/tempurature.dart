c6cacc8b123ef51f11d1ae3f424d7c522eb900c2
Jay <jvanord@indasysllc.com>
2019-07-27 19:48:16 -0400

    temperature view layout

--- metric_converter/lib/views/tempurature.dart
+++ metric_converter/lib/views/tempurature.dart
@@ -5,22 +5,44 @@ class TemperatureWidget extends StatefulWidget {
   _TemperatureWidgetState createState() => _TemperatureWidgetState();
 }
 
-class _TemperatureWidgetState extends State<TemperatureWidget>{
+class _TemperatureWidgetState extends State<TemperatureWidget> {
   double _farenheit = 70;
-  
+  double get _celsius => (_farenheit - 32) * 5 / 9;
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Center(
+      child: Container(
+        padding: EdgeInsets.all(50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '$_farenheit \u00B0F',
-              style: Theme.of(context).textTheme.display1,
+            Card(
+              margin: EdgeInsets.all(20),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  '$_farenheit \u00B0F',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ),
+            ),
+            Divider(),
+            Card(
+              margin: EdgeInsets.all(20),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  '${_celsius.toStringAsFixed(2)} \u00B0C',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ),
             ),
           ],
         ),
-      );
+      ),
+    );
   }
-
 }
