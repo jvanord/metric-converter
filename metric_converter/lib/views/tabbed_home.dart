92368a75e80c0482ca5295d6d8340c9f64253f58
Jay <jvanord@indasysllc.com>
2019-07-27 23:12:22 -0400

    implemented feet/meters screen

--- metric_converter/lib/views/tabbed_home.dart
+++ metric_converter/lib/views/tabbed_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:metric_converter/views/feet.dart';
 import 'package:metric_converter/views/miles.dart';
 import 'package:metric_converter/views/pounds.dart';
 import 'package:metric_converter/views/tempurature.dart';
@@ -26,7 +27,7 @@ class TabbedHomeWidget extends StatelessWidget{
               TemperatureWidget(),
               PoundsWidget(),
               MilesWidget(),
-              Center(child:Text('Short Distance', style: Theme.of(context).textTheme.display2)),
+              FeetWidget(),
             ],
           ),
         ),
